87a8b9978d3120eefa45f2e1d8beb9683e21189b
saidi <saidi.marouen@gmail.com>
2022-09-20 10:46:47 +0100

    add form condidat not finished

--- src/app/components/condidat/condidat.component.ts
+++ src/app/components/condidat/condidat.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AbstractControl, FormGroup } from '@angular/forms';
-import { MenuItem } from 'primeng/api';
+import { ConfirmationService, MenuItem, MessageService } from 'primeng/api';
+import { Table } from 'primeng/table';
+import { Customer, Representative } from 'src/app/api/customer';
+import { Product } from 'src/app/api/product';
+import { CustomerService } from 'src/app/service/customerservice';
+import { ProductService } from 'src/app/service/productservice';
 
 @Component({
   selector: 'app-condidat',
@@ -9,12 +14,12 @@ import { MenuItem } from 'primeng/api';
 })
 export class CondidatComponent implements OnInit {
 
-
-
-
-    
-
-    /* public categories: any = ['B', 'A', 'A1', 'C', 'D', 'B+E', 'C+E', 'D+E', 'H'];
+    customers1: Customer[];
+    loading:boolean = true;
+    @ViewChild('filter') filter: ElementRef;
+    form: FormGroup = new FormGroup({});
+    UserDialog: boolean;
+    public categories: any = ['B', 'A', 'A1', 'C', 'D', 'B+E', 'C+E', 'D+E', 'H'];
     public bureaux: any = ['Menzel temime', 'Nabeul', 'Kélibiya', 'Autres...'];
     valExamen: string;
     valTypeContrat:string;
@@ -23,17 +28,38 @@ export class CondidatComponent implements OnInit {
     selectedDatenaiss:any;
     selectedDateAvSys:any;
     submitted = false;
-    form: FormGroup = new FormGroup({});
     imageSrc: string = '';
-    files:any; */
+    files:any;
 
+    constructor(private customerService: CustomerService){}
     ngOnInit() {
+        this.customerService.getCustomersLarge().then(customers => {
+            this.customers1 = customers;
+            this.loading = false;
 
+            // @ts-ignore
+            this.customers1.forEach(customer => customer.date = new Date(customer.date));
+        });
     }
 
-   /*  onFileChange(event:any) {
-        const reader = new FileReader();
 
+    clear(table: Table) {
+        table.clear();
+        this.filter.nativeElement.value = '';
+    }
+
+    openNew() {
+        //this.user = {};
+        //this.submitted = false;
+        this.UserDialog = true;
+    }
+    hideDialog() {
+        this.UserDialog = false;
+        /* this.updateUserDialog =false;
+        this.submitted = false; */
+    }
+    onFileChange(event:any) {
+        const reader = new FileReader();
         if(event.target.files && event.target.files.length) {
           const [file] = event.target.files;
           reader.readAsDataURL(file);
@@ -46,9 +72,45 @@ export class CondidatComponent implements OnInit {
           };
         }
         this.files = event.target.files[0]
-
+    }
+    get f(): { [key: string]: AbstractControl } {
+        return this.form.controls;
     }
 
+
+
+
+    /* public categories: any = ['B', 'A', 'A1', 'C', 'D', 'B+E', 'C+E', 'D+E', 'H'];
+    public bureaux: any = ['Menzel temime', 'Nabeul', 'Kélibiya', 'Autres...'];
+    valExamen: string;
+    valTypeContrat:string;
+    valTContratCond:string;
+    valPieceFournie: string[] = [];
+    selectedDatenaiss:any;
+    selectedDateAvSys:any;
+    submitted = false;
+    form: FormGroup = new FormGroup({});
+    imageSrc: string = '';
+    files:any;
+
+    ngOnInit() {
+    }
+
+    onFileChange(event:any) {
+        const reader = new FileReader();
+        if(event.target.files && event.target.files.length) {
+          const [file] = event.target.files;
+          reader.readAsDataURL(file);
+
+          reader.onload = () => {
+            this.imageSrc = reader.result as string;
+            this.form.patchValue({
+              fileSource: reader.result
+            });
+          };
+        }
+        this.files = event.target.files[0]
+    }
     get f(): { [key: string]: AbstractControl } {
         return this.form.controls;
     } */
